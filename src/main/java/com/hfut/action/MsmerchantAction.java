710b07c36115ec482f741e52da60081ec620e581
NoteXYX <707997228@qq.com>
2020-04-28 14:32:50 +0800

    2020.4.28

--- src/main/java/com/hfut/action/MsmerchantAction.java
+++ src/main/java/com/hfut/action/MsmerchantAction.java
@@ -5,6 +5,9 @@ import com.hfut.service.MsmerchantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("msmerchantAction")
@@ -12,14 +15,42 @@ public class MsmerchantAction {
 
     @Autowired
     MsmerchantService msmerchantService;
+
     @RequestMapping("toadd")
     public String toadd() {
         return "msmerchant/add";
     }
 
-    @RequestMapping("add")
+    @RequestMapping(value="add", method= RequestMethod.POST)
     public void add(Msmerchant msmerchant) {
         msmerchantService.insertMsmerchant(msmerchant);
         System.out.println(msmerchant);
     }
+
+    @RequestMapping("toupdate")
+    public String toupdate(HttpServletRequest request, int id) {
+        Msmerchant msmerchant = msmerchantService.queryMsmerchantByid(id);
+        request.setAttribute("msmerchant", msmerchant);
+        return "msmerchant/update";
+    }
+
+    @RequestMapping("update")
+    public void update(Msmerchant msmerchant) {
+        msmerchantService.updateMsmerchant(msmerchant);
+        System.out.println(msmerchant);
+    }
+
+    @RequestMapping("del")
+    public void del(HttpServletRequest request, int id) {
+        msmerchantService.deleteMsmerchantByid(id);
+    }
+
+    @RequestMapping("querybyid")
+    public String querybyid(HttpServletRequest request, int id) {
+        Msmerchant msmerchant = msmerchantService.queryMsmerchantByid(id);
+        request.setAttribute("msmerchant", msmerchant);
+        return "msmerchant/view";
+    }
+
+
 }
